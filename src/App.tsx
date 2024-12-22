08a1defc527a5062acd33d9c5db940f74188d5ef
BinyaminYacovson <bj57577@gmail.com>
2024-12-22 19:51:25 +0200

    add Navbar component and implement routing in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './pages/routes';
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <Navbar/>
+      <RouterProvider router={routes} />
+    </>
   )
 }
