c1b6183da9fb3d79b6bba85a87b25874f77534fc
BinyaminYacovson <bj57577@gmail.com>
2024-12-22 19:52:21 +0200

    add routing configuration for Home, Analytics, and Dashboard pages

---
+++ src/pages/routes.tsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom"
+import Home from "./home/Home"
+import Analytics from "./analytics/Analytics"
+import Dashboard from "./dashboard/Dashboard"
+import DeadliestAttackTypesPage from "./analytics/DeadliestAttackTypesPage"
+import IncidentTrendsPage from "./analytics/IncidentTrendsPage"
+// import GroupsByYearPage from "./analytics/GroupsByYearPage"
+
+export const pages = [
+  {
+    path: "",
+    element: <Home />,
+    display: "Home",
+  },
+  {
+    path: "analytics",
+    element: <Analytics />,
+    display: "Analytics",
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard />,
+    display: "Dashboard",
+  },
+  {
+    path: "deadliestAttackTypesPage",
+    element: <DeadliestAttackTypesPage />,
+    display: "DeadliestAttackTypesPage",
+  },
+  {
+    path: "incidentTrendsPage",
+    element: <IncidentTrendsPage />,
+    display: "IncidentTrendsPage",
+  },
+  // {
+  //   path: "groupsByYearPage",
+  //   element: <GroupsByYearPage />,
+  //   display: "GroupsByYearPage",
+  // },
+]
+
+export const routes = createBrowserRouter(pages)
