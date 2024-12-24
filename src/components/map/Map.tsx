5409aa1959085e4ef3871bcb66343a27d36d0f1d
BinyaminYacovson <bj57577@gmail.com>
2024-12-24 16:47:10 +0200

    enhance Map component to support click events and adjust zoom behavior

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -4,17 +4,24 @@ import 'leaflet/dist/leaflet.css';
 
 interface MapProps {
   data: { lat: number; lng: number; label?: string }[];
+  onMapClick?: (point: { lat: number; lng: number }) => void;
 }
 
-const Map: React.FC<MapProps> = ({ data }) => {
+const Map: React.FC<MapProps> = ({ data, onMapClick }) => {
   useEffect(() => {
     if (data.length === 0) return;
 
-    const map = L.map('map');
+    const map = L.map('map').setView([33.8735578, 35.86379], 9);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    if (onMapClick) {
+      map.on('click', (e: L.LeafletMouseEvent) => {
+        onMapClick({ lat: e.latlng.lat, lng: e.latlng.lng });
+      });
+    }
 
     const markers = data.map(point => 
       L.marker([point.lat, point.lng])
@@ -23,7 +30,10 @@ const Map: React.FC<MapProps> = ({ data }) => {
     );
 
     const group = L.featureGroup(markers);
-    map.fitBounds(group.getBounds());
+    map.fitBounds(group.getBounds(), {
+      //  padding: [100, 100],
+       maxZoom: 3
+    });
 
     return () => {
       map.remove();
